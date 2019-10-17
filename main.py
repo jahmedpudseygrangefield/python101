51f112d07cba9a46958d370de0ebe04c28c00664
joseph ahmed <j.ahmed01@pudseygrangefield.co.uk>
2019-10-17 13:04:42 +0100

    v 0.2
    added age input and check

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ print("hello world!")
 name = input("what is your name?  ")
 
 print(f"big up: {name}")
+
+age = int(input("what is your age?  "))
+if age >= 18:
+	print("you are an adult")
+else:
+	print("you are a child")
