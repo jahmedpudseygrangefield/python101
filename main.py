43080f38595cef78880f8501e0fad48905ebea5e
joseph ahmed <j.ahmed01@pudseygrangefield.co.uk>
2019-10-21 14:55:41 +0100

    v 0.3
    added teenager option in if statement

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ name = input("what is your name?  ")
 print(f"big up: {name}")
 
 age = int(input("what is your age?  "))
+
 if age >= 18:
 	print("you are an adult")
+elif age >= 13:
+	print("you are a teenager")
 else:
 	print("you are a child")
